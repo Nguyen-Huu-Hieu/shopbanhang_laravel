126cba442ab833f4e9dffa72588691d4961b5240
Hieu Nguyen <{hieu.nh10@kiotviet.com}>
2021-03-25 20:28:53 +0700

    a

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,6 +1,7 @@
 
 <!DOCTYPE html>
 <head>
+    <p>huu</p>
     <title>Admin - E-shopper</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
